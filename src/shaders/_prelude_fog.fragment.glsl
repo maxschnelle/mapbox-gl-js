be35ef5e0fc986c5668324af2808f0a5f74a7699
Ricky Reusser <ricky.reusser@mapbox.com>
2021-04-02 21:07:32 -0700

    Clean up haze

--- src/shaders/_prelude_fog.fragment.glsl
+++ src/shaders/_prelude_fog.fragment.glsl
@@ -2,11 +2,23 @@
 
 uniform vec2 u_fog_range;
 uniform vec3 u_fog_color;
-uniform vec3 u_haze_color;
+uniform vec3 u_haze_color_linear;
 uniform float u_fog_opacity;
 uniform float u_fog_sky_blend;
 uniform float u_fog_temporal_offset;
 uniform float u_haziness;
+
+// A tone map is a smooth min between x and 1.0
+vec3 tonemap (vec3 color) {
+    // Exponential smooth min
+    const float k = 8.0;
+    return -log2(exp2(-k * color) + exp2(-k)) * (1.0 / k);
+
+    // Cubic smooth min (works well)
+    //const float k = 0.8;
+    //vec3 h = max(k - abs(color - 1.0), vec3(0)) / k;
+    //return max(vec3(0), min(color, vec3(1)) - h * h * h * k * (1.0 / 6.0));
+}
 
 // Assumes z up and camera_dir *normalized* (to avoid computing its length multiple
 // times for different functions).
@@ -56,31 +68,19 @@ vec3 fog_apply_sky_gradient(vec3 camera_ray, vec3 sky_color) {
     return mix(sky_color, u_fog_color, fog_sky_blending(normalize(camera_ray)));
 }
 
-// A tone map is a smooth min between x and 1.0
-vec3 tonemap (vec3 color) {
-    // Exponential smooth min
-    const float k = 8.0;
-    return -log2(exp2(-k * color) + exp2(-k)) * (1.0 / k);
-
-    // Cubic smooth min (works well)
-    //const float k = 0.8;
-    //vec3 h = max(k - abs(color - 1.0), vec3(0)) / k;
-    //return max(vec3(0), min(color, vec3(1)) - h * h * h * k * (1.0 / 6.0));
-}
-
 vec3 fog_apply(vec3 color, vec3 pos) {
     float haze_opac = fog_opacity(pos);
-
     // When haze is present, raise the fog to a power to decrease it
-    float fog_opac = pow(haze_opac, 1.0 / max(0.05, 1.0 - u_haziness));
-    vec3 haze = 1.5 * u_haziness * haze_opac * srgb_to_linear(u_haze_color);
+    float fog_opac = pow(haze_opac, 1.0 + 1.0 / max(0.05, 1.0 - u_haziness));
+    vec3 haze = 1.5 * u_haziness * haze_opac * u_haze_color_linear;
 
-    vec3 col = srgb_to_linear(color);
     // When there's any haze, we prefer the tone map, but when haze goes away,
     // we transition to simply the original color
-    col = mix(col, tonemap(col + haze), sqrt(haze_opac));
+    color = srgb_to_linear(color);
+    color = mix(color, tonemap(color + haze), sqrt(haze_opac));
 
-    return mix(linear_to_srgb(col), u_fog_color, fog_opac);
+    // Mix the fog color to the result so that it still blends with the sky
+    return mix(linear_to_srgb(color), u_fog_color, fog_opac);
 }
 
 // Un-premultiply the alpha, then blend fog, then re-premultiply alpha. For
